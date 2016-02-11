90e060da48e3985433d7951e248c173d45fb7759
Jiansun <jians@kth.se>
2016-02-11 20:57:40 +0100

    sortfunction is done, need to embellish later.

--- src/se/kth/csc/iprog/dinnerplanner/swing/view/IngredientPanel.java
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/IngredientPanel.java
@@ -1,69 +1,89 @@
 package se.kth.csc.iprog.dinnerplanner.swing.view;
 
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
 import se.kth.csc.iprog.dinnerplanner.model.Ingredient;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 import java.awt.*;
-import java.util.ArrayList;
 
-/**
- * Created by Jiansun on 16/2/9.
- */
 public class IngredientPanel extends JPanel {
+    private static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L;
+    Font tableFont=new  Font("Gill Sans MT",Font.PLAIN,20);
 
-	public IngredientPanel() {
+    public IngredientPanel() {
+        super(new GridLayout(1, 0));
+        JTable table = new JTable(new MyTableModel());
+        table.setPreferredScrollableViewportSize(new Dimension(800, 60));
+        table.setFillsViewportHeight(true);
+        table.setAutoCreateRowSorter(true);
 
-        super(new BorderLayout());
-
-
-        ArrayList<Ingredient> tempList = new ArrayList<Ingredient>();
-
-        Ingredient temp1 = new Ingredient("Carrot", 5, "", 3);
-        Ingredient temp2 = new Ingredient("Duck breast", 200, "g", 10);
-
-        tempList.add(temp1);
-        tempList.add(temp2);
-
-        // Table head
-        String[] columnNames = {"Ingredient", "Quality", "Cost"};
-
-        // Table content
-        Object[][] data = {
-                {temp1.getName(), (int)temp1.getQuantity() + temp1.getUnit(), temp1.getPrice() + "$"},
-                {temp2.getName(), (int)temp2.getQuantity() + temp2.getUnit(), temp2.getPrice() + "$"},
-        };
-
-        final JTable table = new JTable(data, columnNames);
-        table.setBackground(Color.orange);
-
+        //Create the scroll pane and add the table to it.
         JScrollPane scrollPane = new JScrollPane(table);
 
+        //Add the scroll pane to this panel.
         add(scrollPane);
+        //table.setFont(tableFont);
+    }
+
+    class MyTableModel extends AbstractTableModel {
+        private String[] columnNames = { "Ingredients", "Quantity", "Cost"};
+
+        private Object[][] data = { { "Egg", "11",  new Integer(5)+"$" },
+                { "Duck", "9",  new Integer(8)+"$" },
+                { "Pork", "18",  new Integer(2)+"$"},
+        };
+
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        public int getRowCount() {
+            return data.length;
+        }
+
+        public String getColumnName(int col) {
+            return columnNames[col];
+        }
+
+        public Object getValueAt(int row, int col) {
+            return data[row][col];
+        }
+//        public boolean isCellEditable(int row, int col) { //Note that the data/cell address is constant,
+//            //no matter where the cell appears onscreen.
+//            if (col < 2) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+//        } /* * Don't need to implement this method unless your table's * data can change. */
+//        }
 
 
+    }
+
+
+    public static void main(String[] args) {
+
+        IngredientPanel newContentPane = new IngredientPanel();
+       	newContentPane.creatAndShowGUI();
     }
 
     public void creatAndShowGUI() {
-        //creat and set up the window
         JFrame frame = new JFrame("Dinner Planner - Ingredients");
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        //Creat and set up the content pane
-
-        IngredientPanel newContentPane = new IngredientPanel();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
-        //display
-
-        //frame.setSize(800, 600);
-        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Create and set up the content pane.
+        //IngredientPanel newContentPane = new IngredientPanel();
+        this.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(this); //Display the window.
+        //frame.pack();
+        frame.setFont(tableFont);
+        frame.setSize(800,600);
         frame.setVisible(true);
     }
-
-    public static void main(String args[]) {
-    	IngredientPanel panel=new IngredientPanel();
-    	panel.creatAndShowGUI();
-    }
 }
+
