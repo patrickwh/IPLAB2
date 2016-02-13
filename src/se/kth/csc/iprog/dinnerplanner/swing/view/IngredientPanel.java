6a55e7c88dd1778142cb94337204db3cc89b76ea
Jiansun <jians@kth.se>
2016-02-13 16:42:25 +0100

    set the table can not be edited

--- src/se/kth/csc/iprog/dinnerplanner/swing/view/IngredientPanel.java
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/IngredientPanel.java
@@ -6,6 +6,7 @@ import se.kth.csc.iprog.dinnerplanner.model.Ingredient;
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 
@@ -14,84 +15,10 @@ public class IngredientPanel extends JPanel {
     private static final long serialVersionUID = 1L;
 
     Font tableFont = new Font("Gill Sans MT", Font.PLAIN, 16);
+    Font tableFontSmall = new Font("Gill Sans MT", Font.PLAIN, 14);
 
 
-    //    public IngredientPanel() {
-//        super(new GridLayout(1, 0));
-//        JTable table = new JTable(new MyTableModel());
-//        table.setPreferredScrollableViewportSize(new Dimension(800, 60));
-//        table.setFillsViewportHeight(true);
-//        table.setAutoCreateRowSorter(true);
-//
-//        //Create the scroll pane and add the table to it.
-//        JScrollPane scrollPane = new JScrollPane(table);
-//
-//        //Add the scroll pane to this panel.
-//        add(scrollPane);
-//        //table.setFont(tableFont);
-//
-//    }
-//
-//    class MyTableModel extends AbstractTableModel {
-//        private String[] columnNames = { "Ingredients", "Quantity", "Cost"};
-//
-//        private Object[][] data = { { "Egg", "11"+" ",  new Integer(5)+"$" },
-//                { "Duck", "300"+"g",  new Integer(8)+"$" },
-//                { "Pork", "180"+"g",  new Integer(2)+"$"},
-//        };
-//
-//
-//
-//        public int getColumnCount() {
-//            return columnNames.length;
-//        }
-//
-//        public int getRowCount() {
-//            return data.length;
-//        }
-//
-//        public String getColumnName(int col) {
-//            return columnNames[col];
-//        }
-//
-//        public Object getValueAt(int row, int col) {
-//            return data[row][col];
-//        }
-//
-//
-//
-//    }
-//
-//    class EvenOddRenderer implements TableCellRenderer {
-//
-//        public  final DefaultTableCellRenderer DEFAULT_RENDERER = new DefaultTableCellRenderer();
-//
-//        public Component getTableCellRendererComponent(JTable table, Object value,
-//                                                       boolean isSelected, boolean hasFocus, int row, int column) {
-//            Component renderer = DEFAULT_RENDERER.getTableCellRendererComponent(
-//                    table, value, isSelected, hasFocus, row, column);
-//            ((JLabel) renderer).setOpaque(true);
-//            Color foreground, background;
-//            if (isSelected) {
-//                foreground = Color.yellow;
-//                background = Color.green;
-//            } else {
-//                if (row % 2 == 0) {
-//                    foreground = Color.blue;
-//                    background = Color.white;
-//                } else {
-//                    foreground = Color.white;
-//                    background = Color.blue;
-//                }
-//            }
-//            renderer.setForeground(foreground);
-//            renderer.setBackground(background);
-//            return renderer;
-//        }
-//    }
-//
-//
-//
+
     public static void main(String[] args) {
 
         IngredientPanel newContentPane = new IngredientPanel();
@@ -112,12 +39,31 @@ public class IngredientPanel extends JPanel {
                 {"Pork", "180" + "g", new Integer(2) + "$"},
         };
         Object headers[] = {"Ingredients", "Quantity", "Cost"};
-
         JTable table = new JTable(rows, headers);
-        table.setFont(tableFont);
+        table.setModel(new DefaultTableModel(rows,headers));
+
+        DefaultTableModel tableModel = new DefaultTableModel(rows,headers){
+            public boolean isCellEditable(int row, int column) {
+                //all cells false
+                return false;
+            }
+        };
+        table.setModel(tableModel);
+        table.setFont(tableFontSmall);
+        table.getTableHeader().setFont(tableFont);
         table.setDefaultRenderer(Object.class, renderer);
         table.setFillsViewportHeight(true);
         table.setAutoCreateRowSorter(true);
+
+
+//        table.setModel(new DefaultTableModel(){
+//            public boolean isCellEdiable(int row,int coloum){
+//                return false;
+//            }
+//        });
+
+
+
         JScrollPane scrollPane = new JScrollPane(table);
         frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
         frame.setFont(tableFont);
@@ -125,9 +71,11 @@ public class IngredientPanel extends JPanel {
         frame.pack();
         frame.setVisible(true);
     }
-//}
 
 
+
+
+    // set the background color
     class EvenOddRenderer implements TableCellRenderer {
 
         public final DefaultTableCellRenderer DEFAULT_RENDERER = new DefaultTableCellRenderer();
